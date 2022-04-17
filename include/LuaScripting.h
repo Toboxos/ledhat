fad9167096ce24dd870178ebe32bbf9e1b1577ab
Tobias Bungard <tobi-bungard@t-online.de>
2022-04-17 11:54:54 +0200

    Exposed LEDHat as global variable to LUA

--- include/LuaScripting.h
+++ include/LuaScripting.h
@@ -3,17 +3,10 @@
 #include <functional>>
 
 namespace LuaScripting {
-    using LoggerFunction = std::function<void(const std::string&)>;
-
     /**
      * Initialize the lua VM
      */
     void init();
-
-    /**
-     * Sets the logging function lua outputs will go to
-     */
-    void setLogger( const LoggerFunction& logger );
 
     /**
      * Executes the given lua code
--- src/LuaScripting.cpp
+++ src/LuaScripting.cpp
@@ -1,5 +1,7 @@
 #include <sstream>
 
+#include "IO.h"
+#include "LEDHat.h"
 #include "LuaScripting.h"
 
 extern "C" {
@@ -14,17 +16,95 @@ namespace LuaScripting {
      */
     static lua_State* L = nullptr;
 
-    /**
-     * Callback to log
-     */
-    static LoggerFunction LOGGER;
+    void log(const std::string& msg);
+    void log(int value);
 
-    void log(const std::string& msg) {
-        if( !LOGGER ) {
-            return;
+    struct Row {
+        static int get(lua_State* L) {
+            const auto col = luaL_checkinteger(L, 2);
+        
+            lua_getfield(L, 1, "row");
+            lua_Integer row = luaL_checkinteger(L, -1);
+            lua_pop(L, 1);
+
+            log("[DEBUG] LEDHat get row="); log(row); log( "col="); log(col); log("\n");
+
+            auto pixel = LEDHat::Instance().getPixel(row - 1, col - 1);
+
+            lua_createtable(L, 0, 0); // -4
+
+            lua_pushinteger(L, pixel.r ); // -3
+            lua_pushinteger(L, pixel.g ); // -2
+            lua_pushinteger(L, pixel.b ); // -1
+
+            lua_rawseti(L, -4, 3); // b
+            lua_rawseti(L, -3, 2); // g
+            lua_rawseti(L, -2, 1); // r
+
+            return 1;
         }
 
-        LOGGER( msg );
+        static int set(lua_State* L) {
+            const auto col = luaL_checkinteger(L, 2);
+        
+            lua_getfield(L, 1, "row");
+            lua_Integer row = luaL_checkinteger(L, -1);
+            lua_pop(L, 1);
+
+            lua_rawgeti(L, 3, 1); // r  -3
+            lua_rawgeti(L, 3, 2); // g  -2 
+            lua_rawgeti(L, 3, 3); // b  -1
+
+            lua_Integer r = luaL_checkinteger(L, -3);
+            lua_Integer g = luaL_checkinteger(L, -2);
+            lua_Integer b = luaL_checkinteger(L, -1);
+
+            lua_pop(L, 3);
+
+            log("[DEBUG] LEDHat set row="); log(row); log( "col="); log(col);
+            log(" r="); log(r); log( " g="); log(g); log(" b="); log(b); log("\n");
+
+
+            LEDHat::Instance().setPixel(row - 1, col - 1, CRGB(r, g, b) );
+            return 0;
+        }
+    };
+
+    struct Matrix {
+        static int index(lua_State* L) {
+            lua_createtable(L, 0, 0);
+            
+            // set row attribute
+            lua_Number row = luaL_checknumber(L, 2);
+            log("[DEBUG] LEDHat get row="); log(row); log("\n");
+
+            lua_pushnumber(L, row);
+            lua_setfield(L, -2, "row");
+
+            // set metatable
+            lua_createtable(L, 0, 0);
+
+            lua_pushcfunction(L, Row::get);
+            lua_setfield(L, -2, "__index");
+
+            lua_pushcfunction(L, Row::set);
+            lua_setfield(L, -2, "__newindex");
+
+            lua_setmetatable(L, -2);
+
+
+            return 1;
+        }
+
+        static int show(lua_State* L) {
+            log("[DEBUG] LEDHat show");
+            LEDHat::Instance().show();
+        }
+    };
+
+
+    void log(const std::string& msg) {
+        IO::write( msg );
     }
 
     void log(int number) {
@@ -39,31 +119,38 @@ namespace LuaScripting {
 
         for( auto i = 1; i <= nargs; ++i ) {
 
-            /* String */
-            if( lua_isstring(L, i) ) {
-               log( luaL_checkstring(L, i) );
-            }
-
-            /* Integer */
-            else if( lua_isinteger(L, i) ) {
-                log( luaL_checkinteger(L, i) );
-            }
+            log( lua_tostring(L, i) );
+            log(" ");
         }
 
         log( "\n" );
         return 1;
     }
 
+    int lua_panic(lua_State* L) {
+        log("Lua error");
+        return 0;
+    }
+
     void init() {
         L = luaL_newstate();
         luaL_openlibs(L);
 
+        lua_atpanic(L, lua_panic);
+
         lua_pushcfunction(L, lua_print);
         lua_setglobal(L, "print");
-    }
 
-    void setLogger(const LoggerFunction& logger) {
-        LOGGER = logger;
+        lua_createtable(L, 0, 0);
+        lua_pushcfunction(L, Matrix::show);
+        lua_setfield(L, -2, "show");
+
+        // create Matrix metatable
+        lua_createtable(L, 0, 0);
+        lua_pushcfunction(L, Matrix::index);
+        lua_setfield(L, -2, "__index");
+        lua_setmetatable(L, -2);
+        lua_setglobal(L, "LEDHat");
     }
 
     void execute(const std::string& code) {
